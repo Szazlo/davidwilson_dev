736f6a5f11844bab1dd4e29cc24da0d30cdd61a5
David Wilson <szabol.david04@gmail.com>
2025-03-08 17:49:25 +0000

    PH Functions

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <Providers>
+              <Providers>
               <div className="flex min-h-screen flex-col">
                 <Navbar />
                 <main className="flex-1 w-full">{children}</main>
--- components/providers/posthog-provider.tsx
+++ components/providers/posthog-provider.tsx
@@ -2,6 +2,7 @@
 
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
+import PostHogPageView from '@/components/providers/PHPageView';
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_API_KEY || '', {
@@ -12,6 +13,7 @@ if (typeof window !== 'undefined') {
 export default function PHProvider({ children }: { children: React.ReactNode }) {
     return (
         <PostHogProvider client={posthog}>
+            <PostHogPageView />
             {children}
         </PostHogProvider>
     )
