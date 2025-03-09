c0e25eb19368b813380fa2ea5b9cd3dcaee7b104
David Wilson <szabol.david04@gmail.com>
2025-03-09 11:46:04 +0000

    Fixes?

--- components/providers/posthog-provider.tsx
+++ components/providers/posthog-provider.tsx
@@ -1,20 +1,22 @@
 'use client'
 
 import posthog from 'posthog-js'
-import { PostHogProvider } from 'posthog-js/react'
-import PostHogPageView from '@/components/providers/PHPageView';
+import { PostHogProvider as PHProvider } from 'posthog-js/react'
+import { useEffect } from 'react'
+import PostHogPageView from './PHPageView'
 
-if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_API_KEY || '', {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || '',
-  })
-}
+export function PostHogProvider({ children }: { children: React.ReactNode }) {
+    useEffect(() => {
+        posthog.init('phc_E52Wqg32qIJQKjTKleXNWMaL7787sgRcEeDmGRP7nxW', {
+            api_host: 'https://eu.i.posthog.com',
+            capture_pageview: false // Disable automatic pageview capture, as we capture manually
+        })
+    }, [])
 
-export default function PHProvider({ children }: { children: React.ReactNode }) {
     return (
-        <PostHogProvider client={posthog}>
+        <PHProvider client={posthog}>
             <PostHogPageView />
             {children}
-        </PostHogProvider>
+        </PHProvider>
     )
 }
